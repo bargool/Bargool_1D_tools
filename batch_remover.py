91519af128b733f3acd6edf9b77a481b9ba9a13b
Alexey Nakoryakov <alexey.nakoryakov@gms.ru>
2015-03-25 15:09:50 +0300

    Minor refactoring and error removing

--- batch_remover.py
+++ batch_remover.py
@@ -22,7 +22,7 @@ bl_info = {
     'name': 'Batch Remover',
     'author': 'Aleksey Nakoryakov, Paul Kotelevets aka 1D_Inc (concept design)',
     'category': 'Object',
-    'version': (0, 9, 0),
+    'version': (0, 9, 1),
     'location': 'View3D > Toolbar',
     'category': 'Mesh'
 }
@@ -350,19 +350,20 @@ class ImportCleanupOperator(bpy.types.Operator):
     bl_options = { 'REGISTER', 'UNDO' }
     
     def execute(self, context):
-        mallas = [o for o in objetos if o.type == 'MESH' and o.is_visible(scn)]
-        obj= context.selected_objects
-        for ob in obj:
-            if ob.type == 'MESH':
-                ob.select = True
-                context.scene.objects.active = ob
-                bpy.ops.object.mode_set(mode='EDIT')
-                bpy.ops.mesh.select_all(action='SELECT')
-                bpy.ops.mesh.remove_doubles(threshold=0.0001, use_unselected=False)
-                bpy.ops.mesh.tris_convert_to_quads(limit=0.698132, uvs=False, vcols=False, sharp=False, materials=False)
-                # remove uvs
-                bpy.ops.mesh.normals_make_consistent(inside=False )
-                bpy.ops.object.mode_set(mode='OBJECT')
+        scene = context.scene
+        objects = [o for o in context.selected_objects if o.type == 'MESH' and o.is_visible(scene)]
+        for ob in objects:
+            ob.select = True
+            context.scene.objects.active = ob
+            bpy.ops.object.mode_set(mode='EDIT')
+            bpy.ops.mesh.select_all(action='SELECT')
+            bpy.ops.mesh.remove_doubles(threshold=0.0001, use_unselected=False)
+            bpy.ops.mesh.tris_convert_to_quads(limit=0.698132, uvs=False, vcols=False, sharp=False, materials=False)
+            # remove uvs
+            bpy.ops.mesh.normals_make_consistent(inside=False)
+            bpy.ops.object.mode_set(mode='OBJECT')
+        
+        return {'FINISHED'}
 
 def get_description(operator):
     """ Gets description from operator, if exists """
